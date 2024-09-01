8d4a4dac44f8f3160580f7d297d2605eb02eb209
PandaRetro <v.h.vice@gmail.com>
2024-09-01 22:27:05 +1200

    legs dont move at the same time

--- 1.Protoype/IK_Arm_wBody_test.gd
+++ 1.Protoype/IK_Arm_wBody_test.gd
@@ -1,25 +1,28 @@
 extends CharacterBody3D
 
-var base
-var cur_targ
+var base = []
+var cur_targ = []
 var new_targ = []
 var tween
+var curve
+var grounded = []
+var N0_points
+var gravity = 0 #ProjectSettings.get_setting("physics/3d/default_gravity")
+
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-var N0_points
-var jump_dist = 3
-var gravity = 0 #ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _ready():
 	base = $Points/Base.get_children()
-	cur_targ = $Points/Targets.get_children()
 	N0_points = len(base)
 	for i in range(N0_points):
+		cur_targ.append($Points/Targets.get_child(i).get_child(0).get_child(0))
 		cur_targ[i].position = to_global(base[i].position)
 		new_targ.append(cur_targ[i].position)
+		grounded.append(true)
 
 
 func _physics_process(delta):
@@ -44,21 +47,27 @@ func _physics_process(delta):
 	for i in range(N0_points):
 		var diff = velocity
 		diff.y = 0
-		if (to_global(base[i].position) - cur_targ[i].position).length() > jump_dist:
-			new_targ[i] = (to_global(base[i].position) + (diff.normalized() * jump_dist * 0.5))
-
-		if cur_targ[i].position != new_targ[i]:
-			#cur_targ[i].position = lerp(cur_targ[i].position, new_targ[i], 0.2)
-			_animate(cur_targ[i], new_targ[i])
+		if (to_global(base[i].position) - cur_targ[i].position).length() > (diff.length() * 0.25):
+			if grounded[i - 1] == true and grounded[(i + 1) % len(cur_targ)] == true:
+				grounded[i] = false
+				var intermediate = to_global(base[i].position)
+				intermediate.y += 2
+				new_targ[i] = (to_global(base[i].position) + (diff.normalized() * diff.length() * 0.25))
+				_animate(cur_targ, new_targ, intermediate, i)
 
 
 	move_and_slide()
 
-func _animate(point, new_targ):
-	#if tween:
-		#tween.kill()
+
+func _animate(cur_targ, new_targ, intermediate, index):
+	
+	curve = Curve3D.new()
+	curve.add_point(cur_targ[index].position, cur_targ[index].position, intermediate)
+	curve.add_point(new_targ[index], intermediate, new_targ[index])
+	$Points/Targets.get_child(index).curve = curve
 	tween = create_tween()
-	tween.tween_property(point, "position", new_targ, 0.5)
-	tween.TRANS_EXPO
-	tween.EASE_OUT
+	#tween.tween_property(point[index], "position", intermediate, 0.05)
+	tween.tween_property(cur_targ[index], "position", new_targ[index], 0.1)
+	await tween.finished
+	grounded[index] = true
 	#tween.interpolate_value(point.position, new_targ, 0, 0.5, Tween.TRANS_EXPO, Tween.EASE_OUT)
