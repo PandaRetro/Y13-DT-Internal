66e958eff98d87b6589908ba3a8ee8b405044cad
PandaRetro <v.h.vice@gmail.com>
2024-10-23 20:35:00 +1300

    Cant walk of edge, title screen, started saving

--- 3.Alpha/Scripts/Camera.gd
+++ 3.Alpha/Scripts/Camera.gd
@@ -9,8 +9,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position.x = char.position.x
+	self.position.x = lerp(self.position.x, char.position.x, 0.05)
 	var z = self.position.z - char.position.z
 	var y = self.position.y - char.position.y
 	var angle = atan(z/y)
-	self.rotation.x = -((PI/2) - angle)
+	self.rotation.x = lerp(self.rotation.x, -((PI/2) - angle), 0.05)
--- 3.Alpha/Scripts/Character_body.gd
+++ 3.Alpha/Scripts/Character_body.gd
@@ -1,5 +1,9 @@
 extends CharacterBody3D
 
+var save_path = "user://variable.save"
+
+var self_position
+var ray_casts
 var tween
 var average_points
 var bases = []
@@ -12,7 +16,6 @@ const SPEED = 3.0
 
 
 func _ready():
-	#bases = $Points/Bases.get_children()
 	for i in range(N0_LEGS):
 		var arm = $Arms.get_children()[i]
 		bases.append(arm.get_child(1))
@@ -26,52 +29,51 @@ func _physics_process(delta):
 
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
+	if direction and _ray_cast(direction.normalized() + self.position):
 		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * SPEED
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	#main loop for all leg movement
+	#Main loop for all leg movement
 	average_points = 0
 	for i in range(len(bases)): 
 		average_points += cur_targ[i].position.y
 		var diff = velocity
 		diff.y = 0
 
-		#adjust base positions
+		#Adjust base positions
 		if _ray_cast(bases[i].global_position) != {}: 
 			bases[i].global_position = _ray_cast(bases[i].global_position)["position"]
 
-		#checks for movement, and animation
-		#if (bases[i].global_position - cur_targ[i].position).length() > (diff.length() * 0.25) and \
-		#grounded[i - 1] == true and \
-		#grounded[(i + 1) % len(cur_targ)] == true and \
-		#grounded[i] == true:
-		if (bases[i].global_position - cur_targ[i].position).length() > (diff.length() * 0.25) and \
-		grounded[i - 2] == true and \
-		grounded[(i + 2) % len(cur_targ)] == true and \
-		grounded[i] == true:
+		#Leg movement checks
+		if (
+				(bases[i].global_position - cur_targ[i].position).length() > (diff.length() * 0.25) 
+				and grounded[i - 2] == true
+				and grounded[(i + 2) % len(cur_targ)] == true
+				and grounded[i] == true
+		):
 			grounded[i] = false
 			new_targ[i] = (bases[i].global_position + (diff.normalized() * diff.length() * 0.25))
 
-			#adjust target positions
+			#Adjust target positions
 			if _ray_cast(new_targ[i]) != {}:
 				new_targ[i] = _ray_cast(new_targ[i])["position"]
 
+			#Choose intermediate position
 			var intermediate = bases[i].global_position
 			intermediate.y += ((cur_targ[i].position - new_targ[i]).length()) / 4
 
-			#animate
+			#Animate
 			tween = create_tween()
 			tween.tween_property(cur_targ[i], "position", intermediate, 0.1)
 			tween.tween_property(cur_targ[i], "position", new_targ[i], 0.1)
 			tween.tween_callback(func(): grounded[i] = true)
 
-	#rotation and position for body
+	#Rotation and position for body
 	self.position.y = lerp(self.position.y, _ray_cast(self.position - Vector3(0, 2, 0))["position"].y + 1.25, 0.1)
-	#self.position.y = lerp(self.position.y, (average_points/8) + 1.25, 0.3)
 	if velocity.length()> 0.1 :
 		self.rotation.y = lerp_angle(self.rotation.y, atan2(velocity.x, velocity.z), 0.05)
 
@@ -86,3 +88,14 @@ func _ray_cast(point):
 	var result = get_world_3d().direct_space_state.intersect_ray(params)
 	return(result)
 
+func _save():
+	self_position = self.position
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_var(self_position)
+	
+func _load_data():
+	if FileAccess.file_exists(save_path):
+		var file = FileAccess.open(save_path, FileAccess.READ)
+		self.position = file.get_var(self_position)
+	else:
+		self.postion = Vector3(0, 1.5, 0)
