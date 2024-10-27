8846251dcd3cd711f886b99eff747586266eb219
PandaRetro <v.h.vice@gmail.com>
2024-10-27 22:39:28 +1300

    lil fixes

--- 3.Alpha/Scripts/Camera.gd
+++ 3.Alpha/Scripts/Camera.gd
@@ -1,7 +1,7 @@
 extends Camera3D
 
 var char
-
+var lerp_weight = 0.05
 
 func _ready():
 	char = get_node("/root/Main/Character")
@@ -9,8 +9,8 @@ func _ready():
 
 func _process(delta):
 	# Follow player and rotate to keep in frame
-	self.position.x = lerp(self.position.x, char.position.x, 0.05)
+	self.position.x = lerp(self.position.x, char.position.x, lerp_weight)
 	var z = self.position.z - char.position.z
 	var y = self.position.y - char.position.y
 	var angle = atan(z/y)
-	self.rotation.x = lerp(self.rotation.x, -((PI/2) - angle), 0.05)
+	self.rotation.x = lerp(self.rotation.x, -((PI/2) - angle), lerp_weight)
